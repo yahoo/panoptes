7438d2937d5850d6452b4c74bd7aca7112bc7fe1
iholmes <iholmes@yahoo-inc.com>
2018-10-04 17:06:38 -0500

    Add tests for framework/plugins/helpers.py

---
+++ tests/test_framework_plugins_helpers.py
@@ -0,0 +1,32 @@
+import unittest
+
+from mock import create_autospec, patch
+
+from yahoo.contrib.panoptes.framework.plugins.helpers import expires, time_limit
+from yahoo.contrib.panoptes.framework.plugins.panoptes_base_plugin import PanoptesPluginInfo
+
+
+class TestFrameworkPlugins(unittest.TestCase):
+    def test_helpers(self):
+        mock_panoptes_plugin_info = create_autospec(PanoptesPluginInfo)
+        mock_panoptes_plugin_info.execute_frequency = 60
+
+        mock_plugin_agent_plugin_expires_multiple = 2
+        with patch('yahoo.contrib.panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE',
+                   mock_plugin_agent_plugin_expires_multiple):
+            self.assertEqual(expires(mock_panoptes_plugin_info), 60*2)
+            self.assertNotEqual(expires(mock_panoptes_plugin_info), 60*3)
+
+        with self.assertRaises(AssertionError):
+            expires(PanoptesPluginInfo)
+
+        mock_plugin_agent_plugin_time_limit_multiple = 1.25
+        with patch('yahoo.contrib.panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE',
+                   mock_plugin_agent_plugin_time_limit_multiple):
+            self.assertEqual(time_limit(mock_panoptes_plugin_info), 60*1.25)
+            mock_panoptes_plugin_info.execute_frequency = 5
+            self.assertNotEqual(time_limit(mock_panoptes_plugin_info), 5*1.25)
+            self.assertEqual(time_limit(mock_panoptes_plugin_info), round(5*1.25))
+
+        with self.assertRaises(AssertionError):
+            time_limit(PanoptesPluginInfo)
