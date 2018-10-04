0b104ea187e2d0cd736b9f402ce8fe7782ae380d
iholmes <iholmes@yahoo-inc.com>
2018-10-04 18:26:28 -0500

    Fix paths for patching in tests.

--- tests/test_framework_plugins_helpers.py
+++ tests/test_framework_plugins_helpers.py
@@ -12,7 +12,7 @@ class TestFrameworkPlugins(unittest.TestCase):
         mock_panoptes_plugin_info.execute_frequency = 60
 
         mock_plugin_agent_plugin_expires_multiple = 2
-        with patch('yahoo.contrib.panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE',
+        with patch('yahoo_panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE',
                    mock_plugin_agent_plugin_expires_multiple):
             self.assertEqual(expires(mock_panoptes_plugin_info), 60*2)
             self.assertNotEqual(expires(mock_panoptes_plugin_info), 60*3)
@@ -21,7 +21,7 @@ class TestFrameworkPlugins(unittest.TestCase):
             expires(PanoptesPluginInfo)
 
         mock_plugin_agent_plugin_time_limit_multiple = 1.25
-        with patch('yahoo.contrib.panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE',
+        with patch('yahoo_panoptes.framework.plugins.helpers.const.PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE',
                    mock_plugin_agent_plugin_time_limit_multiple):
             self.assertEqual(time_limit(mock_panoptes_plugin_info), 60*1.25)
             mock_panoptes_plugin_info.execute_frequency = 5
