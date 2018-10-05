b087ef83211d03be76a5e0afda982486207cb8ac
iholmes <iholmes@yahoo-inc.com>
2018-10-05 17:02:16 -0500

    Make tests more concise. Add comments

--- tests/test_framework_plugins_helpers.py
+++ tests/test_framework_plugins_helpers.py
@@ -1,7 +1,9 @@
 import unittest
 
-from mock import create_autospec, patch
+from mock import create_autospec
 
+from yahoo_panoptes.framework.const import PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE, \
+    PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE
 from yahoo_panoptes.framework.plugins.helpers import expires, time_limit
 from yahoo_panoptes.framework.plugins.panoptes_base_plugin import PanoptesPluginInfo
 
@@ -9,17 +11,24 @@ from yahoo_panoptes.framework.plugins.panoptes_base_plugin import PanoptesPlugin
 class TestFrameworkPlugins(unittest.TestCase):
     def test_helpers(self):
         mock_panoptes_plugin_info = create_autospec(PanoptesPluginInfo)
-        mock_panoptes_plugin_info.execute_frequency = 60
+        mock_panoptes_plugin_info.execute_frequency = 5.75
 
-        self.assertEqual(expires(mock_panoptes_plugin_info), round(60 * 2))
+        #  Assert 'round' is called by testing against non-integer product
+        self.assertNotEqual(expires(mock_panoptes_plugin_info),
+                            mock_panoptes_plugin_info.execute_frequency * PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE)
+        self.assertEqual(expires(mock_panoptes_plugin_info),
+                         round(mock_panoptes_plugin_info.execute_frequency * PLUGIN_AGENT_PLUGIN_EXPIRES_MULTIPLE))
 
+        #  Test failure when called on a non-instance of PanoptesPluginInfo
         with self.assertRaises(AssertionError):
             expires(PanoptesPluginInfo)
 
-        self.assertEqual(time_limit(mock_panoptes_plugin_info), round(60 * 1.25))
-        mock_panoptes_plugin_info.execute_frequency = 5
-        self.assertNotEqual(time_limit(mock_panoptes_plugin_info), 5 * 1.25)
-        self.assertEqual(time_limit(mock_panoptes_plugin_info), round(5 * 1.25))
+        #  Assert 'round' is called by testing against non-integer product
+        self.assertNotEqual(time_limit(mock_panoptes_plugin_info),
+                            mock_panoptes_plugin_info.execute_frequency * PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE)
+        self.assertEqual(time_limit(mock_panoptes_plugin_info),
+                         round(mock_panoptes_plugin_info.execute_frequency * PLUGIN_AGENT_PLUGIN_TIME_LIMIT_MULTIPLE))
 
+        #  Test failure when called on a non-instance of PanoptesPluginInfo
         with self.assertRaises(AssertionError):
             time_limit(PanoptesPluginInfo)
