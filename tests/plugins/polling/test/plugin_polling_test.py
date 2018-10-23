534c0d25ac3046f3cba6a2c7688a22cc6c0b7f44
iholmes <iholmes@yahoo-inc.com>
2018-10-23 16:35:32 -0500

    Add basic tests for PanoptesPluginRunner and PanoptesPluginWithEnrichmentRunner.

--- tests/plugins/polling/test/plugin_polling_test.py
+++ tests/plugins/polling/test/plugin_polling_test.py
@@ -1,6 +1,23 @@
 from yahoo_panoptes.polling.polling_plugin import PanoptesPollingPlugin
+from yahoo_panoptes.framework.metrics import PanoptesMetric, PanoptesMetricType, PanoptesMetricsGroup, \
+    PanoptesMetricsGroupSet
+from yahoo_panoptes.framework.resources import PanoptesResource
+
+
+_TEST_INTERVAL = 60
 
 
 class PanoptesTestPollingPlugin(PanoptesPollingPlugin):
+    name = "Panoptes Test Polling Plugin"
+    panoptes_resource = PanoptesResource(resource_site='test', resource_class='test',
+                                         resource_subclass='test',
+                                         resource_type='test', resource_id='test', resource_endpoint='test',
+                                         resource_plugin='test')
+
     def run(self, context):
-        super(PanoptesTestPollingPlugin, self).run(context)
+        metric_group_set = PanoptesMetricsGroupSet()
+        metric1 = PanoptesMetric("test", 0.0, PanoptesMetricType.GAUGE)
+        metric_group = PanoptesMetricsGroup(self.panoptes_resource, "Test", _TEST_INTERVAL)
+        metric_group.add_metric(metric1)
+        metric_group_set.add(metric_group)
+        return metric_group_set
