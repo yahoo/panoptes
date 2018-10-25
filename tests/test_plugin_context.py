df785cccdc6c1a9074da0b78639eec7b2f331e53
iholmes <iholmes@yahoo-inc.com>
2018-10-25 14:05:39 -0500

    Break out get_test_conf_file into new tests/helpers.py file. Replace dunder on properties with single-underscore.

--- tests/test_plugin_context.py
+++ tests/test_plugin_context.py
@@ -7,7 +7,6 @@ This module implements a context object that is passed to each plugin during exe
 The context of plugin contains it's configuration, the logger it should use, the key/value store it can use and an
 optional, arbitrary data object to be passed to the plugin
 """
-import os
 import unittest
 
 from mock import patch
@@ -16,6 +15,7 @@ from yahoo_panoptes.framework.plugins.context import PanoptesPluginContext, Pano
 from yahoo_panoptes.framework.resources import PanoptesContext
 
 from .test_framework import PanoptesTestKeyValueStore, panoptes_mock_redis_strict_client
+from .helpers import get_test_conf_file
 
 plugin_conf = {
     'Core': {
@@ -36,37 +36,30 @@ class TestEnrichment:
 class TestPanoptesPluginContexts(unittest.TestCase):
     @patch('redis.StrictRedis', panoptes_mock_redis_strict_client)
     def setUp(self):
-        self.my_dir, self.panoptes_test_conf_file = _get_test_conf_file()
-        self.__panoptes_context = PanoptesContext(self.panoptes_test_conf_file)
+        self.my_dir, self.panoptes_test_conf_file = get_test_conf_file()
+        self._panoptes_context = PanoptesContext(self.panoptes_test_conf_file)
 
     def test_panoptes_plugin_context(self):
-        test_kv_store = PanoptesTestKeyValueStore(self.__panoptes_context)
-        test_secrets_store = PanoptesTestKeyValueStore(self.__panoptes_context)
-        panoptes_plugin_context = PanoptesPluginContext(self.__panoptes_context, "test logger", plugin_conf,
+        test_kv_store = PanoptesTestKeyValueStore(self._panoptes_context)
+        test_secrets_store = PanoptesTestKeyValueStore(self._panoptes_context)
+        panoptes_plugin_context = PanoptesPluginContext(self._panoptes_context, "test logger", plugin_conf,
                                                         test_kv_store, test_secrets_store, {'test_key': 'test_value'})
 
-        self.assertEqual(panoptes_plugin_context.logger, self.__panoptes_context.logger.getChild("test logger"))
+        self.assertEqual(panoptes_plugin_context.logger, self._panoptes_context.logger.getChild("test logger"))
         self.assertEqual(panoptes_plugin_context.config, plugin_conf)
         self.assertEqual(panoptes_plugin_context.kv, test_kv_store)
         self.assertEqual(panoptes_plugin_context.secrets, test_secrets_store)
         self.assertEqual(panoptes_plugin_context.data, {'test_key': 'test_value'})
-        self.assertSetEqual(panoptes_plugin_context.sites, self.__panoptes_context.config_object.sites)
-        self.assertDictEqual(panoptes_plugin_context.snmp, self.__panoptes_context.config_object.snmp_defaults)
+        self.assertSetEqual(panoptes_plugin_context.sites, self._panoptes_context.config_object.sites)
+        self.assertDictEqual(panoptes_plugin_context.snmp, self._panoptes_context.config_object.snmp_defaults)
 
     def test_panoptes_plugin_with_enrichment_context(self):
-        test_kv_store = PanoptesTestKeyValueStore(self.__panoptes_context)
-        test_secrets_store = PanoptesTestKeyValueStore(self.__panoptes_context)
+        test_kv_store = PanoptesTestKeyValueStore(self._panoptes_context)
+        test_secrets_store = PanoptesTestKeyValueStore(self._panoptes_context)
         test_enrichment = TestEnrichment()
-        panoptes_plugin_with_enrichment_context = PanoptesPluginWithEnrichmentContext(self.__panoptes_context,
+        panoptes_plugin_with_enrichment_context = PanoptesPluginWithEnrichmentContext(self._panoptes_context,
                                                                                       "test logger", plugin_conf,
                                                                                       test_kv_store, test_secrets_store,
                                                                                       {'test_key': 'test_value'},
                                                                                       test_enrichment)
         self.assertEqual(panoptes_plugin_with_enrichment_context.enrichment, test_enrichment)
-
-
-def _get_test_conf_file():
-    my_dir = os.path.dirname(os.path.realpath(__file__))
-    panoptes_test_conf_file = os.path.join(my_dir, 'config_files/test_panoptes_config.ini')
-
-    return my_dir, panoptes_test_conf_file
