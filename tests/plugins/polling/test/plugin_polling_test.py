f2f09454db0dbd5c570c447d1c41a7e4b34b4935
iholmes <iholmes@yahoo-inc.com>
2018-10-26 14:44:20 -0500

    Add copyright header.

--- tests/plugins/polling/test/plugin_polling_test.py
+++ tests/plugins/polling/test/plugin_polling_test.py
@@ -1,3 +1,10 @@
+"""
+Copyright 2018, Oath Inc.
+Licensed under the terms of the Apache 2.0 license. See LICENSE file in project root for terms.
+
+This module defines a generic polling plugin class for use in testing.
+"""
+
 from yahoo_panoptes.polling.polling_plugin import PanoptesPollingPlugin
 from yahoo_panoptes.framework.metrics import PanoptesMetric, PanoptesMetricType, PanoptesMetricsGroup, \
     PanoptesMetricsGroupSet
