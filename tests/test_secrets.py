0c6c56b926383543fe4656c8572b122fe6108dc1
iholmes <iholmes@yahoo-inc.com>
2018-11-02 14:37:55 -0700

    Add copyright notice to test_secrets.py

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,3 +1,7 @@
+"""
+Copyright 2018, Oath Inc.
+Licensed under the terms of the Apache 2.0 license. See LICENSE file in project root for terms.
+"""
 import unittest
 
 from mock import patch, MagicMock
