d32934651e9d0fc604485541948c3e9b17de75b8
iholmes <iholmes@yahoo-inc.com>
2019-06-05 17:57:04 -0500

    Add generic snmp polling plugin and basic test files.

---
+++ tests/plugins/polling/generic/snmp/test_generic_snmp_polling_plugin.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+
+from tests.plugins.helpers import SNMPPollingPluginTestFramework, setup_module_default, tear_down_module_default
+from yahoo_panoptes.plugins.polling.generic.snmp.plugin_polling_generic_snmp import \
+    PluginPollingGenericSNMPMetrics
+
+module_path = os.path.dirname(os.path.abspath(__file__))
+
+
+def setUpModule():
+    setup_module_default(module_path)
+
+
+def tearDownModule():
+    tear_down_module_default()
+
+
+class TestPluginPollingGenericSNMPFeatures(SNMPPollingPluginTestFramework, unittest.TestCase):
+    plugin_instance = PluginPollingGenericSNMPMetrics()
+    path = os.path.dirname(os.path.abspath(__file__))
+    plugin_conf = {
+        'Core': {
+            'name': 'Test Plugin',
+            'module': 'test_plugin'
+        },
+        'main': {
+            'execute_frequency': '60',
+            'enrichment_ttl': '300',
+            'resource_filter': 'resource_class = "network"',
+            'namespace': 'metrics',
+            'polling_status_metric_name': 'polling_status'
+        },
+        'snmp': {
+            'timeout': 10,
+            'retries': 1,
+            'non_repeaters': 0,
+            'max_repetitions': 25,
+        },
+        'enrichment': {
+            'preload': 'self:metrics'
+        }
+    }
+
+    def test_inactive_port(self):
+        pass
