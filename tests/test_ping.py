8d6ce5c78d77d51e4b25851881ef688eaae23214
Nathan Kamm <nathan.kamm@verizonmedia.com>
2019-10-03 16:50:19 -0700

    Polling Status Tests

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -9,6 +9,12 @@ import subprocess
 import json  # why isn't the call below recognized?
 
 from yahoo_panoptes.framework.utilities.ping import *
+from yahoo_panoptes.framework.resources import PanoptesResource, PanoptesContext
+from yahoo_panoptes.plugins.polling.utilities.polling_status import PanoptesPollingStatus, \
+    DEVICE_METRICS_STATES
+from yahoo_panoptes.framework.metrics import PanoptesMetricsGroup
+
+from helpers import get_test_conf_file
 
 # For testing locally on OSX
 TEST_PING_RESPONSE = "ping statistics ---\n" \
@@ -17,6 +23,7 @@ TEST_PING_RESPONSE = "ping statistics ---\n" \
 
 
 class TestPing(unittest.TestCase):
+
     def test_code_output(self):
         # Mock the system calls
         subprocess.check_output = Mock(return_value=TEST_PING_RESPONSE)
@@ -82,3 +89,77 @@ class TestPing(unittest.TestCase):
         with self.assertRaises(PanoptesPingException):
             test_unknown_host_ping = PanoptesPing(hostname=hostname)
             self.assertDictEqual(test_fail_d, test_unknown_host_ping.response)
+
+
+class TestPollingStatus(unittest.TestCase):
+
+    def setUp(self):
+
+        self.my_dir, self.panoptes_test_conf_file = get_test_conf_file()
+        self._panoptes_context = PanoptesContext(self.panoptes_test_conf_file)
+        self._panoptes_resource = PanoptesResource(resource_site='test',
+                                                   resource_class='test',
+                                                   resource_subclass='test',
+                                                   resource_type='test',
+                                                   resource_id='test',
+                                                   resource_endpoint='test',
+                                                   resource_plugin='test',
+                                                   resource_creation_timestamp=0,
+                                                   resource_ttl=180)
+        self.polling_status = PanoptesPollingStatus(resource=self._panoptes_resource,
+                                                    execute_frequency=180,
+                                                    logger=self._panoptes_context.logger)
+
+    def test_polling_status_initial_settings(self):
+
+        self.assertEqual(self.polling_status.device_status, DEVICE_METRICS_STATES.SUCCESS)
+        self.assertEqual(self.polling_status.device_name, 'test')
+        self.assertEqual(self.polling_status.device_type, 'test:test:test')
+
+    def test_polling_failure_1(self):
+
+        ERROR_PING_RESPONSE = "ping statistics ---\n" \
+                              "3 packets transmitted, 0 received, 100% packet loss, time 1439ms\n" \
+                              "rtt min/avg/max/mdev = 0.000/0.00/0.0/0.00 ms"
+
+        subprocess.check_output = Mock(return_value=ERROR_PING_RESPONSE)
+
+        self.polling_status._device_status = DEVICE_METRICS_STATES.TIMEOUT
+
+        device_status = self.polling_status.device_status_metrics_group
+
+        self.assertEqual(
+            json.loads(device_status.json)['metrics'][0]['metric_value'],
+            DEVICE_METRICS_STATES.PING_FAILURE
+        )
+
+    def test_polling_state_failure(self):
+
+        self.polling_status.handle_success('Interface')
+        self.polling_status.handle_success('Test')
+
+        self.assertEqual(self.polling_status._metric_statuses,
+            {'Interface': 0, 'Test': 0})
+
+        self.polling_status.handle_exception('Interface', DEVICE_METRICS_STATES.TIMEOUT)
+
+        self.assertEqual(self.polling_status._metric_statuses,
+            {'Interface': 4, 'Test': 0})
+
+        self.assertEqual(self.polling_status.device_status, 4)
+
+    def test_polling_state_internal_failure(self):
+        self.polling_status.handle_exception('Interface', 10000)
+
+        self.assertEqual(self.polling_status._device_status,
+                         DEVICE_METRICS_STATES.INTERNAL_FAILURE)
+
+    def test_polling_state_success(self):
+
+        self.polling_status.handle_success('Interface')
+        self.polling_status.handle_success('Test')
+
+        self.assertEqual(self.polling_status._metric_statuses,
+            {'Interface': 0, 'Test': 0})
+
+        self.assertEqual(self.polling_status.device_status, 0)
