ef1aad5134d92b0b50c65747753cde82c4ca8c3a
Nathan Kamm <nathan.kamm@verizonmedia.com>
2019-11-14 14:50:41 -0800

    Ascii encode. Fixes #137

--- yahoo_panoptes/plugins/polling/interface/plugin_polling_device_interface_metrics.py
+++ yahoo_panoptes/plugins/polling/interface/plugin_polling_device_interface_metrics.py
@@ -84,10 +84,14 @@ class PluginPollingDeviceInterfaceMetrics(PanoptesSNMPBasePlugin, PanoptesPollin
         return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index).get(u'interface_name')
 
     def get_alias(self, interface_index):
-        return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index).get(u'alias')
+        return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index) \
+            .get(u'alias') \
+            .encode(u'ascii', u'ignore')
 
     def get_description(self, interface_index):
-        return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index).get(u'description')
+        return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index) \
+            .get(u'description') \
+            .encode(u'ascii', u'ignore')
 
     def get_media_type(self, interface_index):
         return self.enrichment.get_enrichment_value(u'self', u'interface', interface_index).get(u'media_type')
